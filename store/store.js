6cb4bbdabc4d4e08023a5dfa402e4598d6978855
Darrenjoe <491361994@qq.com>
2019-09-08 10:05:57 +0800

    redux

---
+++ store/store.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+const initialState = {
+  count: 0
+};
+
+{
+    type:
+}
+
+const ADD = "ADD";
+
+function reducer(state = initialState, action) {
+  switch (action) {
+    case ADD:
+      return { count: state.count + 1 };
+      break;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer, initialState);
