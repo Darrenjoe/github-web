3a11baf4a60b9e5545a3b3be36cebcc4e6cf53a7
Darrenjoe <491361994@qq.com>
2019-09-09 20:10:27 +0800

    server

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const next = require("next");
+const seesion = require("koa-session");
+const Redis = require("ioredis");
+
+const RedisSessionStore = require("./server/session-store");
 
 const dev = process.env.NODE_ENV != "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+// 创建Redis-Client
+const redis = new Redis();
+
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
+
+  server.keys = ["Darren develop Github App"];
+  const SESSION_CONFIG = {
+    key: "jid",
+    store: new RedisSessionStore(redis)
+  };
+  server.use(session(SESSION_CONFIG, server));
 
   router.get("/a/:id", async ctx => {
     const id = ctx.params.id;
@@ -21,6 +35,7 @@ app.prepare().then(() => {
   server.use(router.routes());
 
   server.use(async (ctx, next) => {
+    ctx.cookies.set("id", "userid:123sdas");
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
   });
