ae2b0815bcf2c59c1e780e4511c9866c47864b46
Darrenjoe <491361994@qq.com>
2019-09-19 21:50:21 +0800

    hooks

--- pages/b.js
+++ pages/b.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 
 class MyCount extends React.Component {
   state = {
@@ -22,13 +22,27 @@ class MyCount extends React.Component {
   }
 }
 
+function countReducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return state + 1;
+    case "minus":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
 function MyCountFunc() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, disppatchCount] = useReducer(countReducer, 0);
 
   useEffect(() => {
     const inerval = setInterval(() => {
-      setCount(c => c + 1);
+      // setCount(c => c + 1);
+      disppatchCount({ type: "minus" });
     }, 1000);
+    return () => clearInterval(inerval);
   }, []);
   return <span>{count}</span>;
 }
