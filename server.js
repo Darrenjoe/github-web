fd12a34b04a20eae17810453e71a7e0db6f8c592
Darrenjoe <491361994@qq.com>
2019-10-04 17:09:45 +0800

    session

--- server.js
+++ server.js
@@ -17,22 +17,41 @@ app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
 
-  server.use(async (ctx, next) => {
-    console.log(ctx.cookies.get("id"));
-    ctx.session = ctx.session || {};
-    ctx.session.user = {
-      username: "Darren",
-      age: 23
-    };
-    await next();
-  });
-
   server.keys = ["Darren develop Github App"];
   const SESSION_CONFIG = {
     key: "jid",
     store: new RedisSessionStore(redis)
   };
   server.use(session(SESSION_CONFIG, server));
+
+  // server.use(async (ctx, next) => {
+  //   console.log(ctx.cookies.get("id"));
+  //   // 获取用户数据
+  //   // 比如调用 `model/getUserById(id)`
+  //   // ctx.session = ctx.session || {};
+  //   // ctx.session.user = {
+  //   //   username: "Darren",
+  //   //   age: 23
+  //   // };
+
+  //   if (!ctx.session.user) {
+  //     ctx.session.user = {
+  //       name: "darren",
+  //       age: 18
+  //     };
+  //   } else {
+  //     console.log("session is:", ctx.session);
+  //   }
+
+  //   await next();
+  // });
+
+  server.use(async (ctx, next) => {
+    if (ctx.cookies.get("jid")) {
+      ctx.session = {};
+    }
+    await next();
+  });
 
   router.get("/a/:id", async ctx => {
     const id = ctx.params.id;
@@ -42,6 +61,22 @@ app.prepare().then(() => {
     });
     ctx.respond = false;
   });
+
+  router.get("/set/user", async ctx => {
+    // const id = ctx.params.id;
+    // await handle(ctx.req, ctx.res, {
+    //   pathname: "/a",
+    //   query: { id }
+    // });
+    ctx.respond = false;
+
+    ctx.session.user = {
+      username: "Darren",
+      age: 23
+    };
+    ctx.body = "set session success";
+  });
+
   server.use(router.routes());
 
   server.use(async (ctx, next) => {
