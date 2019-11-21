c8d54a0c1e711bb6739f03c9456160da59f85bed
Darrenjoe <491361994@qq.com>
2019-11-21 22:24:17 +0800

    loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,43 @@
 import App, { Container } from "next/app";
 import { Provider } from "react-redux";
+import Router from "next/router";
+import Link from "next/link";
 
 import Layout from "../components/Layout";
 import "antd/dist/antd.css";
+import PageLoading from "../components/PageLoading";
 
 import testHoc from "../lib/with-redux";
 
 class MyApp extends App {
   state = {
-    context: "value"
+    context: "value",
+    loading: false
   };
+
+  startLoading = () => {
+    this.setState({
+      loading: true
+    });
+  };
+  stopLoading = () => {
+    this.setState({
+      loading: false
+    });
+  };
+
+  componentDidMount() {
+    Router.events.on("routeChangeStart", this.startLoading);
+    Router.events.on("routeChangeComplete", this.stopLoading);
+    Router.events.on("routeChangeError", this.stopLoading);
+  }
+
+  componentWillMount() {
+    Router.events.off("routeChangeStart", this.startLoading);
+    Router.events.off("routeChangeComplete", this.stopLoading);
+    Router.events.off("routeChangeError", this.stopLoading);
+  }
+
   static async getInitialProps(ctx) {
     const { Component } = ctx;
     let pageProps = {};
@@ -27,7 +55,14 @@ class MyApp extends App {
     return (
       <Container>
         <Provider store={reduxStore}>
+          {this.state.loading ? <PageLoading /> : null}
           <Layout>
+            <Link href="/">
+              <a>Index</a>
+            </Link>
+            <Link href="/detail">
+              <a>Detail</a>
+            </Link>
             <Component {...pageProps}></Component>
           </Layout>{" "}
         </Provider>
