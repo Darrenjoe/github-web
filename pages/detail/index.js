c0a6aa8c635d1d52c47cbd697dff5581805279a1
Darrenjoe <491361994@qq.com>
2019-12-03 22:01:34 +0800

    issues

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,54 +1,13 @@
-import Repo from "../../components/Repo";
-import Link from "next/link";
+import withRepoBasic from "../../components/with-repo-basic";
 
-import api from "../../lib/api";
-
-function Detail({ repoBasic }) {
-  return (
-    <div className="root">
-      <div className="repo-basic">
-        <Repo repo={repoBasic} />
-        <div className="tabs">
-          <Link href="/detail">
-            <a className="tab index">Readme</a>
-          </Link>
-          <Link href="/detail/issues">
-            <a className="tab issues">Issues</a>
-          </Link>
-        </div>
-      </div>
-      <div>Readme</div>
-      <style jsx>{`
-        .root {
-          padding-top: 20px;
-        }
-        .repo-basic {
-          padding: 20px;
-          border: 1px solid #eee;
-          margin-bottom: 20px;
-          border-radius: 5px;
-        }
-        .tab + .tab {
-          margin-left: 20px;
-        }
-      `}</style>
-    </div>
-  );
+function Detail({ text }) {
+  return <span>Detail Index {text} </span>;
 }
 
-Detail.getInitialProps = async ({ router, ctx }) => {
-  const { owner, name } = ctx.query;
-
-  const repoBasic = await api.request(
-    {
-      url: `/repos/${owner}/${name}`
-    },
-    ctx.req,
-    ctx.res
-  );
+Detail.getInitialProps = async () => {
   return {
-    repoBasic: repoBasic.data
+    text: "1234"
   };
 };
 
-export default Detail;
+export default withRepoBasic(Detail, "index");
