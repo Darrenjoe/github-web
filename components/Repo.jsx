bf84e20326fa43b18656015455dad60362d958a2
Darrenjoe <491361994@qq.com>
2019-12-05 22:18:48 +0800

    issues

--- components/Repo.jsx
+++ components/Repo.jsx
@@ -1,12 +1,9 @@
 import Link from "next/link";
 import { Icon } from "antd";
-import moment from "moment";
+import { getLastUpdated } from "../lib/util";
 
 function getLicense(license) {
   return license ? `${license.spdx_id} license` : "";
-}
-function getLastUpdated(time) {
-  return moment(time).fromNow();
 }
 
 export default ({ repo }) => {
--- pages/detail/issues.js
+++ pages/detail/issues.js
@@ -1,56 +1,102 @@
-import { Avatar } from "antd";
+import { useState, useCallback } from "react";
+import { Avatar, Button } from "antd";
+import dynamic from "next/dynamic";
+import { getLastUpdated } from "../../lib/util";
+
 import withRepoBasic from "../../components/with-repo-basic";
 
 import api from "../../lib/api";
 
-function IssuesItem({ issue }) {
+const MdRender = dynamic(() => import("../../components/MarkDownRender"));
+
+function IssueDetail({ issue }) {
   return (
-    <div className="issue">
-      <div className="avatar">
-        <Avatar src={issue.user.avatar} shape="square" size={50} />
-      </div>
-      <div className="main-info">
-        <h6>
-          <span>{issue.title}</span>
-        </h6>
-        <p className="sub-info">
-          <span>Updated at {issue.updated_at}</span>
-        </p>
+    <div className="root">
+      <MdRender content={issue.body} />
+      <div className="actions">
+        <Button href={issue.html_url} target="_blank">
+          打开Issue讨论页面
+        </Button>
       </div>
       <style jsx>{`
-        .issue {
-          display: flex;
-          position: relative;
-          padding: 10px;
+        .root {
+          background: #fefefe;
+          padding: 20px;
         }
-        .issue:hover {
-          background: #fafafa;
-        }
-        .issue + .issue {
-          border-top: 1px solid #eee;
-        }
-        .main-info > h6 {
-          max-with: 600px;
-          font-size: 20px;
-        }
-        .avatar {
-          margin-right: 20px;
-        }
-        .sub-info {
-          margin-bottom: 0;
-        }
-        .sub-info > span + span {
-          display: inline-block;
-          margin-left: 20px;
-          font-size: 12px;
+        .actions {
+          text-align: right;
         }
       `}</style>
     </div>
   );
 }
 
+function IssuesItem({ issue }) {
+  const [showDetail, setShowDetail] = useState(false);
+
+  const toggleShowDetail = useCallback(() => {
+    setShowDetail(detail => !detail);
+  }, []);
+
+  return (
+    <div>
+      <div className="issue">
+        <Button
+          type="primary"
+          size="small"
+          style={{ position: "absolute", right: 10, top: 10 }}
+          onClick={toggleShowDetail}
+        >
+          {showDetail ? "隐藏" : "查看"}
+        </Button>
+        <div className="avatar">
+          <Avatar src={issue.user.avatar} shape="square" size={50} />
+        </div>
+        <div className="main-info">
+          <h6>
+            <span>{issue.title}</span>
+          </h6>
+          <p className="sub-info">
+            <span>Updated at {getLastUpdated(issue.updated_at)}</span>
+          </p>
+        </div>
+
+        <style jsx>{`
+          .issue {
+            display: flex;
+            position: relative;
+            padding: 10px;
+          }
+          .issue:hover {
+            background: #fafafa;
+          }
+          .issue + .issue {
+            border-top: 1px solid #eee;
+          }
+          .main-info > h6 {
+            max-with: 600px;
+            font-size: 20px;
+            padding-right: 40px;
+          }
+          .avatar {
+            margin-right: 20px;
+          }
+          .sub-info {
+            margin-bottom: 0;
+          }
+          .sub-info > span + span {
+            display: inline-block;
+            margin-left: 20px;
+            font-size: 12px;
+          }
+        `}</style>
+      </div>
+      {showDetail ? <IssueDetail issue={issue} /> : null}
+    </div>
+  );
+}
+
 function Issues({ issues }) {
-  console.log(issues);
   return (
     <div className="root">
       <div className="issues">
@@ -58,6 +104,14 @@ function Issues({ issues }) {
           <IssuesItem issue={issue} key={issue.id} />
         ))}
       </div>
+      <style jsx>{`
+        .issues {
+          border: 1px solid #eee;
+          border-radius: 5px;
+          margin-bottom: 20px;
+          margin-top: 20px;
+        }
+      `}</style>
     </div>
   );
 }
